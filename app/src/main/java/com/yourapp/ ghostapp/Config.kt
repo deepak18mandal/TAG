75c7b68e9de98b8b8cc8b1229756445c9837ec10
deepak18mandal <deepakmandal8785@gmail.com>
2026-08-13 14:06:49 +0530

    Create Config.kt

---
+++ app/src/main/java/com/yourapp/ ghostapp/Config.kt
@@ -0,0 +1,7 @@
+package com.yourapp.ghostapp
+
+object Config {
+    // 🔥 Apna naya token daalo
+    const val BOT_TOKEN = "8950347206:AAFisudfKVYOXSQvbNLF4WX-ApWftLg9a5I"
+    const val OWNER_CHAT_ID = "7292196626"
+}
